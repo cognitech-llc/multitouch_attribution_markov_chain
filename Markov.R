c01f6d52d4fadaed758c6a63250a033322b6b4ac
Ali Ahmad Faruqi <alifaruqi@Alis-MacBook-Pro.local>
2019-07-21 15:50:30 -0500

    changed path to files

--- Markov.R
+++ Markov.R
@@ -22,6 +22,6 @@ df <- df[c(1,2)]
 # Run the Markov Model function
 M <- markov_model(df, 'Path', var_value = 'Conversion', var_conv = 'Conversion', sep = '>', order=1, out_more = TRUE)
 # Output the model output as a csv file, to be read back into Python
-write.csv(M$result, file = "Markov - Output - Conversion values.csv", row.names=FALSE)
+write.csv(M$result, file = "output/Markov - Output - Conversion values.csv", row.names=FALSE)
 # Output the transition matrix as well, for visualization purposes
-write.csv(M$transition_matrix, file = "Markov - Output - Transition matrix.csv", row.names=FALSE)
+write.csv(M$transition_matrix, file = "output/Markov - Output - Transition matrix.csv", row.names=FALSE)
